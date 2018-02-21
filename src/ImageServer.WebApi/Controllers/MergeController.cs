12e571771ed122f2c0b2cb19b9584a8fd1cc3100
Saturn Technologies <info@saturn72.com>
2018-02-21 12:07:13 +0200

    sett pixels to add values

--- src/ImageServer.WebApi/Controllers/MergeController.cs
+++ src/ImageServer.WebApi/Controllers/MergeController.cs
@@ -23,61 +23,72 @@ namespace ImageServer.WebApi.Controllers
                     data = request
                 });
 
-            var secondaries = request.SubImages.Select(s => Image.Load(s.Uri));
+            var secondaries = request.SubImages.ToDictionary(k=>k, v => Image.Load(v.Uri));
 
             using (var primary = Image.Load(request.Primary))
             {
-
-                //ignore multiple elements
                 var secondary = secondaries.ElementAt(0);
 
-                var overlapPercentage = (int)(100 - request.SubImages.ElementAt(0).Overlap) / 100;
-                //Create empty filr for output
-                int width = primary.Width + secondary.Width * overlapPercentage;
-                var heightAddition = GetHeightAddition(request.SubImages);
-                int height = primary.Height + secondary.Height * overlapPercentage;
+                var pixelsToAdd = GetHeightValueToAdd(secondaries);
+                var height = primary.Height + pixelsToAdd.addToTop + pixelsToAdd.addToBottom;
+                var width = primary.Width + pixelsToAdd.addToLeft + pixelsToAdd.addToRight;
 
-                using (var output = new Image<Rgba32>(width, height))
+                using (var output = new Image<Rgba32>((int)width, (int)height))
                 {
-
                     // do your drawing in here...
                 }
-
-                //get overlap
-                //get location
-
-                // image is now in a file format agnositic structure in memory as a series of Rgba32 pixels
-                //image.Mutate(ctx => ctx.Resize(image.Width / 2, image.Height / 2)); // resize the image in place and return it for chaining
-
-                var outDir = "output";
-                if (!Directory.Exists(outDir))
-                    Directory.CreateDirectory(outDir);
-                Save(outDir + "\\primary", primary, request.Primary);
-                for (int i = 0; i < secondaries.Count(); i++)
-                    Save(outDir + "\\secondary_" + i, secondaries.ElementAt(i), 
-                        request.SubImages.ElementAt(i).Uri);
-            }   
+            }
 
             for (int i = 0; i < secondaries.Count(); i++)
-                secondaries.ElementAt(i).Dispose();
+                secondaries.ElementAt(i).Value.Dispose();
 
             throw new NotImplementedException();
         }
 
-        private int[] GetHeightAddition(IEnumerable<SubImage> subImages)
+        private (uint addToTop, uint addToBottom, uint addToRight, uint addToLeft) GetHeightValueToAdd(IDictionary<SubImage, Image<Rgba32>> subImages)
         {
-            var result = new [] { 0, 0 };
-            var siLocations = subImages.Select(si => (SubImageLocation)si.MergeLocation);
+            var imageToAddToTop = subImages.Where(si =>
+            {
+                var ml = (SubImageLocation)si.Key.MergeLocation;
+                return ml == SubImageLocation.TopLeft
+                    || ml == SubImageLocation.TopMiddle
+                    || ml == SubImageLocation.TopRight;
+            });
+            var addToTop = GetMax(imageToAddToTop);
 
-            //Choose max overlap from bottom and top
-            if (siLocations.Any(slBotton =>
-            slBotton == SubImageLocation.BottomLeft ||
-            slBotton == SubImageLocation.BottomMiddle ||
-            slBotton == SubImageLocation.BottomRight))
-                result[0] = 
+            var imageToAddToBottom= subImages.Where(si =>
+            {
+                var ml = (SubImageLocation)si.Key.MergeLocation;
+                return ml == SubImageLocation.BottomLeft
+                    || ml == SubImageLocation.BottomMiddle
+                    || ml == SubImageLocation.BottomRight;
+            });
+            var addToBottom = GetMax(imageToAddToBottom);
 
+            var imageToAddToRight= subImages.Where(si =>
+            {
+                var ml = (SubImageLocation)si.Key.MergeLocation;
+                return ml == SubImageLocation.Right;
+            });
+            var addToRight= GetMax(imageToAddToRight);
 
-            throw new NotImplementedException();
+            var imageToAddToLeft = subImages.Where(si =>
+            {
+                var ml = (SubImageLocation)si.Key.MergeLocation;
+                return ml == SubImageLocation.Left;
+            });
+            var addToLeft= GetMax(imageToAddToLeft);
+
+            return (addToTop, addToBottom, addToRight, addToLeft);
+        }
+
+        private uint GetMax(IEnumerable<KeyValuePair<SubImage, Image<Rgba32>>> imagesData)
+        {
+            return imagesData.Any() ?
+                (uint)imagesData
+                .Select(s => s.Key.Overlap * s.Value.Height / 100)
+                .Max()
+                : 0;
         }
 
         private void Save(string path, Image<Rgba32> image, string origPath)
